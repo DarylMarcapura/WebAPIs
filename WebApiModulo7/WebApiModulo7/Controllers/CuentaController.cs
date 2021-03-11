bbb80f58ec7151b9d6525c8f2bc980213379894a
Daryl Marcapura <ddk_sk@hotmail.com>
2021-03-10 20:06:32 -0500

    migracion de ef

---
+++ WebApiModulo7/WebApiModulo7/Controllers/CuentaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiModulo7.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CuentaController : ControllerBase
+    {
+        public CuentaController()
+        {
+
+        }
+    }
+}
