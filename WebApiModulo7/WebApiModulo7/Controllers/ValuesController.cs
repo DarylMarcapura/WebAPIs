b38b6e553867de24225799545cf308baa0b0609e
Daryl Marcapura <ddk_sk@hotmail.com>
2021-03-12 19:38:45 -0500

    avance de capitulo 6 parte 2

---
+++ WebApiModulo7/WebApiModulo7/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiModulo7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //Añadir autenticacion de , solo usuarios con rol admin pueden utilizar
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "admin")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDataProtectionProvider protectionProvider;
+
+        public ValuesController(IDataProtectionProvider protectionProvider, HashService hashService )
+        {
+            this.protectionProvider = protectionProvider;
+        }
+    }
+}
