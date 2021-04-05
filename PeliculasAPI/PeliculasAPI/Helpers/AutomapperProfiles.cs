8225fc4820cb9ebf1cbd0f0bc3aff063aac9aa25
Daryl <ddk_sk@hotmail.com>
2021-04-04 22:14:51 -0500

    agregado proyecto nuevo, avance de controlador de peliculas api

---
+++ PeliculasAPI/PeliculasAPI/Helpers/AutomapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Helpers
+{
+    public class AutomapperProfiles : Profile
+    {
+        public AutomapperProfiles()
+        {
+            //mapeando las conversiones de entidades y dtos
+            CreateMap<Genero, GeneroDTO>().ReverseMap();
+        }
+    }
+}
